9ea7feeff76292a9e56c6a17c5c51ede013dd2eb
Roman Ondráček <mail@romanondracek.cz>
2024-04-09 09:08:53 +0200

    Add linear_search function
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- searching.py
+++ searching.py
@@ -19,9 +19,28 @@ def read_data(file_name: str, field: str) -> Union[list[int], str, None]:
     return data[field] if field in data else None
 
 
+def linear_search(sequence: list[int], target: int) -> dict:
+    """
+    Linear search algorithm.
+    :param sequence: (list), list of integers
+    :param target: (int), target value to search
+    :return: (dict), dictionary with search results information
+    """
+    result = {
+        'positions': [],
+        'count': 0,
+    }
+    for index, item in enumerate(sequence):
+        if item == target:
+            result['positions'].append(index)
+            result['count'] += 1
+    return result
+
+
 def main():
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
+    print(linear_search(sequential_data, 5))
 
 
 if __name__ == '__main__':
