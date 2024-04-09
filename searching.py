455036b3b5677fb1428bd3fd82c36d67dce8f6f5
Roman Ondráček <mail@romanondracek.cz>
2024-04-09 08:53:38 +0200

    Add read_data function
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- searching.py
+++ searching.py
@@ -1,10 +1,12 @@
 import os
+import json
+from typing import Union
 
 # get current working directory path
 cwd_path = os.getcwd()
 
 
-def read_data(file_name, field):
+def read_data(file_name: str, field: str) -> Union[list[int], str, None]:
     """
     Reads json file and returns sequential data.
     :param file_name: (str), name of json file
@@ -12,10 +14,14 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data[field] if field in data else None
 
 
 def main():
-    pass
+    sequential_data = read_data('sequential.json', 'unordered_numbers')
+    print(sequential_data)
 
 
 if __name__ == '__main__':
